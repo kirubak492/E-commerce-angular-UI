f0a593544d11b7036927c6cfe7c06a260db99da3
kiruba <kirubak492@gmail.com>
2025-08-23 16:20:48 +0530

    removed unused product variable

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -12,26 +12,6 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit{
 
-product1:any={
-  "_id": "689a1483d6decfb210614d15",
-  "name": "Wireless Mouse",
-  "price": "799",
-  "description": "Ergonomic wireless mouse with USB receiver.",
-  "ratings": "4.5",
-  "images": [
-    {
-      "image": "assets/images/products/1.jpg"
-    },
-    {
-      "image": "mouse2.jpg"
-    }
-  ],
-  "category": "Electronics",
-  "seller": "TechZone",
-  "stock": "146",
-  "numOfReviews": "230",
-  "createAt": "2025-08-01T10:00:00Z"
-}
 
 product:any={}
 
